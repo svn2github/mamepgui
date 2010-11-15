svn2github/mamepgui

commit 7f3f7f3adb96ef72f24941cd9eebb3606351cc73

options rewrite #1
git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus/trunk/mamepgui@4801 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

gamelist.cpp
@@ -1526,10 +1526,6 @@ void Gamelist::init(bool toggleState, int initMethod)
 			if (pGuiSettings->contains(optName))
 				pMameOpt->globalvalue = pGuiSettings->value(optName).toString();
 		}
-
-		// we're ready to set version info
-//		if (!hasInitd)
-//			win->setVersion();
 	}
 
 	//localization must be loaded after init of options so that proper lang directory can be located
@@ -1585,7 +1581,7 @@ void Gamelist::init(bool toggleState, int initMethod)
 		if (pGuiSettings->value("column_state").isValid())
 			column_state = pGuiSettings->value("column_state").toByteArray();
 		else
-			column_state = defSettings.value("column_state").toByteArray();
+			column_state = defaultGuiSettings.value("column_state").toByteArray();
 
 		win->tvGameList->header()->restoreState(column_state);
 		restoreFolderSelection();

mameopt.h
@@ -154,7 +154,7 @@ public:
 class OptInfo : public QObject
 {
 public:
-	QListWidget *lstCatView;
+	QListWidget *optCatView;
 	QTreeView *optView;
 	QStandardItemModel *optModel;
 
@@ -167,6 +167,7 @@ Q_OBJECT
 
 public:
 	OptionUtils(QObject *parent = 0);
+	//init option-related GUI
 	void init();
 	QVariant getField(const QModelIndex &, int);
 	const QString getLongName(QString);
@@ -180,7 +181,7 @@ public:
 	void saveIniFile(int , const QString &);
 
 public slots:
-	void preUpdateModel(QListWidgetItem *currItem = 0, int optType = -1, const QString &gameName = "", int method = 0);
+	void preUpdateModel(QListWidgetItem *currItem = 0, int optLevel = -1, QString gameName = "", int method = 0);
 	void updateHeaderSize(int, int, int);
 
 private:
@@ -190,10 +191,10 @@ private:
 
 	void loadIni(int, const QString &);
 	void loadTemplate();
-	QHash<QString, QString> parseIniFile(const QString &);
+	QHash<QString, QString> parseIni(QTextStream &in, bool isInitOptCatMap);
 	void addModelItemTitle(QStandardItemModel*, QString);
 	void addModelItem(QStandardItemModel*, QString);
-	void updateModel(QString, int);
+	void updateModel(const QString&, int);
 };
 
 extern OptionUtils *optUtils;

mamepgui.pro
@@ -8,25 +8,17 @@ QT += xml
 INCLUDEPATH += quazip lzma include include/zlib include/SDL include/SDL/$${OSDIR}
 
 #LIBS
+TARGETDEPS += \
+	./lib/$${OSDIR}/libquazip.a \
+	./lib/$${OSDIR}/liblzma.a
+
 LIBS += -L./lib/$${OSDIR}
 LIBS += -lquazip -llzma
 
-build_static {
-  QTPLUGIN += qico qjpeg
-  DEFINES += USE_STATIC
-}
-
 build_sdl {
-  macx: LIBS += -framework Cocoa -framework IOKit -framework CoreAudio -framework AudioToolbox -framework AudioUnit -framework OpenGL -framework ForceFeedback
-  LIBS += -lSDL
-  DEFINES += USE_SDL
-}
-
-win32: RC_FILE = mamepgui.rc
-
-macx {
-DEFINES += _LZMA_UINT32_IS_ULONG
-ICON = mamepgui.icns
+	macx: LIBS += -framework Cocoa -framework IOKit -framework CoreAudio -framework AudioToolbox -framework AudioUnit -framework OpenGL -framework ForceFeedback
+	LIBS += -lSDL
+	DEFINES += USE_SDL
 }
 
 HEADERS += \
@@ -74,3 +66,9 @@ TRANSLATIONS = \
 	lang/mamepgui_ru_RU.ts
 
 RESOURCES = mamepgui_main.qrc
+
+win32: RC_FILE = mamepgui.rc
+
+macx {
+	ICON = mamepgui.icns
+}

common_settings.pri
@@ -3,18 +3,23 @@ macx:OSDIR       = macx
 unix:!macx:OSDIR = linux
 
 CONFIG(debug, debug|release){
-	OBJECTS_DIR       = obj/debug
+	OBJECTS_DIR = tmp/debug
 }else{
-	OBJECTS_DIR       = obj/release
-	DEFINES          *= QT_NO_DEBUG_OUTPUT
+	OBJECTS_DIR = tmp/release
+	CONFIG += build_static
+	CONFIG += build_sdl
+	DEFINES *= QT_NO_DEBUG_OUTPUT
 }
 
-MOC_DIR = moc
-RCC_DIR = rcc
-UI_DIR = ui
+build_static {
+	QTPLUGIN += qico qjpeg
+	DEFINES += USE_STATIC
+}
 
-CONFIG += build_static
-CONFIG += build_sdl
+MOC_DIR = tmp
+RCC_DIR = tmp
+UI_DIR = tmp
+
 macx {
 CONFIG += x86 ppc
 }

lzma/lzma.pro
@@ -4,6 +4,10 @@ DESTDIR = ../lib/$${OSDIR}
 TARGET = lzma
 TEMPLATE = lib
 
+macx {
+	DEFINES += _LZMA_UINT32_IS_ULONG
+}
+
 HEADERS += \
 	7zBuf.h \
 	7zCrc.h \

mamepgui_main.cpp
@@ -33,7 +33,7 @@ QString CFG_PREFIX =
 
 MainWindow *win;
 QSettings *pGuiSettings;
-QSettings defSettings(":/res/mamepgui" INI_EXT, QSettings::IniFormat);
+QSettings defaultGuiSettings(":/res/mamepgui" INI_EXT, QSettings::IniFormat);
 QString currentAppDir;
 QString mame_binary;
 QString language;
@@ -456,8 +456,7 @@ void MainWindow::init()
 		return;
 	}
 
-	initSettings();
-	loadSettings();
+	loadGuiSettings();
 	
 	if (!validateMameBinary())
 	{
@@ -1017,11 +1016,10 @@ void MainWindow::showRestartDialog()
 		close();
 }
 
-void MainWindow::initSettings()
+void MainWindow::loadGuiSettings()
 {
     pGuiSettings->setFallbacksEnabled(false);
 
-	//remove invalid settings
 	validGuiSettings = (QStringList() 
 		<< "snapshot_directory"	//fixme: core
 		<< "flyer_directory"
@@ -1039,42 +1037,67 @@ void MainWindow::initSettings()
 		<< "icons_directory"
 		<< "background_directory"
 		<< "folder_directory"
-		<< "background_file"
+
+		//m1
 		<< "m1_directory"
 		<< "m1_language"
+		//ips
 		<< "ips_language"
 		<< "ips_relationship"
-		<< "gui_style"
-		<< "background_stretch"
+
+		//ui path
 		<< "mame_binary"
-		<< "option_geometry"
+
+		//ui mainwindow
+		<< "background_file"
+		<< "background_stretch"
+		<< "gui_style"
+
+		//ui gamelist
+		<< "zoom_icon"
+		<< "list_mode"
+		<< "local_game_list"
 		<< "sort_column"
 		<< "sort_reverse"
 		<< "default_game"
+
+		//ui folder
 		<< "default_folder"
 		<< "hide_folders"
 		<< "folder_flag"
-		<< "vertical_tabs"
+
+		//ui snapshot
 		<< "stretch_screenshot_larger"
 		<< "enforce_aspect"
-		<< "zoom_icon"
-		<< "local_game_list"
-		<< "list_mode"
-		<< "option_column_state"
+		<< "vertical_tabs"
+
 		<< "window_geometry"
 		<< "window_state"
 		<< "column_state"
+		<< "option_geometry"
+		<< "option_column_state"
 		<< "language");
 
+	//remove invalid settings
 	QStringList keys = pGuiSettings->allKeys();
 	foreach(QString key, keys)
 	{
+		//fixme: ignore mess for now
 		if (key.endsWith("_extra_software") || validGuiSettings.contains(key))
 			continue;
 
 		pGuiSettings->remove(key);
 	}
 
+	//assign settings to local variables
+	//fixme: move loadLayout and other similar methods
+	currentGame = pGuiSettings->value("default_game").toString();
+
+	//fixme: remove default by setting it programatically
+	if (pGuiSettings->value("option_column_state").isValid())
+		option_column_state = pGuiSettings->value("option_column_state").toByteArray();
+	else
+		option_column_state = defaultGuiSettings.value("option_column_state").toByteArray();
 }
 
 void MainWindow::loadLayout()
@@ -1131,16 +1154,6 @@ void MainWindow::loadLayout()
 	else
 		actionBgStretch->setChecked(true);
 	
-}
-
-void MainWindow::loadSettings()
-{
-	currentGame = pGuiSettings->value("default_game").toString();
-
-	if (defSettings.value("option_column_state").isValid())
-		option_column_state = defSettings.value("option_column_state").toByteArray();
-	else
-		option_column_state = pGuiSettings->value("option_column_state").toByteArray();
 }
 
 void MainWindow::saveSettings()

mamepgui_main.h
@@ -53,6 +53,7 @@ public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+	void saveSettings();
 	bool isDockTabVisible(QString);
 	void setVersion();
 	void enableCtrls(bool);
@@ -140,10 +141,6 @@ public slots:
 	void logStatus(QString);
 	void logStatus(GameInfo *);
 	void init();
-	void initSettings();
-	void loadLayout();
-	void loadSettings();
-	void saveSettings();
 	void setDockOptions();
 	void setGuiStyle(QString = "");
 	void setBgTile();
@@ -159,6 +156,8 @@ private:
 	QString gui_style;
 	QDockWidget *dwHistory;
 
+	void loadGuiSettings();
+	void loadLayout();
 	bool validateMameBinary();
 	QString selectMameBinary();
 	void initHistory(int);
@@ -193,7 +192,7 @@ private:
 extern QString CFG_PREFIX;
 
 extern MainWindow *win;
-extern QSettings *pGuiSettings, defSettings;
+extern QSettings *pGuiSettings, defaultGuiSettings;
 extern QString currentAppDir;
 extern QString mame_binary;
 extern QString language;

commit 119dd4a62cf7fc7b27777e276ecf3d851f979a43

fixed regressions in r4801
git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus/trunk/mamepgui@4803 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

gamelist.cpp
@@ -1513,7 +1513,7 @@ void Gamelist::init(bool toggleState, int initMethod)
 		optUtils->loadDefault(pMameDat->defaultIni);
 
 		// load mame.ini overrides
-		optUtils->preUpdateModel(NULL, OPTLEVEL_GLOBAL, currentGame, 1);
+		optUtils->chainLoadOptions(NULL, OPTLEVEL_GLOBAL, currentGame, 1);
 
 		// load GUI path overrides
 		foreach (QString optName, mameOpts.keys())
@@ -1591,7 +1591,7 @@ void Gamelist::init(bool toggleState, int initMethod)
 	win->tvGameList->setSortingEnabled(true);
 
 	//fixme: hack to update snapshot_directory for non-Windows build
-	optUtils->preUpdateModel(NULL, OPTLEVEL_GLOBAL, currentGame, 1);
+	optUtils->chainLoadOptions(NULL, OPTLEVEL_GLOBAL, currentGame, 1);
 
 	if (!hasInitd)
 	{
@@ -3385,7 +3385,7 @@ void Gamelist::runMame(int method, QStringList playArgs)
 		QStringList strOptions;
 
 		//update mameOpts
-		optUtils->preUpdateModel(NULL, OPTLEVEL_CURR, currentGame, 1);
+		optUtils->chainLoadOptions(NULL, OPTLEVEL_CURR, currentGame, 1);
 
 		QStringList optNames = mameOpts.keys();
 		optNames.sort();

ips.cpp
@@ -418,7 +418,7 @@ void IpsUI::updateList()
 	twList->expandAll();
 
 	//update mameOpts
-	optUtils->preUpdateModel(NULL, OPTLEVEL_CURR, currentGame, 1);
+	optUtils->chainLoadOptions(NULL, OPTLEVEL_CURR, currentGame, 1);
 
 	//check the selected ips
 	if (mameOpts.contains("ips"))

mameopt.cpp
@@ -58,7 +58,7 @@ void OptionsUI::init(int optLevel, int lstRow)
 {
 	//init ctlrs
 	for (int i = OPTLEVEL_GUI; i < OPTLEVEL_LAST; i++)
-		optUtils->preUpdateModel(NULL, i);
+		optUtils->chainLoadOptions(NULL, i);
 
 	//select option tab
 	tabOptions->setCurrentIndex(optLevel);
@@ -1247,10 +1247,10 @@ void OptionUtils::init()
 	}
 
 	for (int i = OPTLEVEL_GUI; i < OPTLEVEL_LAST; i++)
-		connect(win->optionsUI->optCtrls[i], SIGNAL(currentItemChanged(QListWidgetItem *, QListWidgetItem *)), this, SLOT(preUpdateModel(QListWidgetItem *)));
+		connect(win->optionsUI->optCtrls[i], SIGNAL(currentItemChanged(QListWidgetItem *, QListWidgetItem *)), this, SLOT(chainLoadOptions(QListWidgetItem *)));
 
 	connect(win->optionsUI, SIGNAL(accepted()), &optDelegate, SLOT(setChangesAccepted()));
-	connect(win->optionsUI->tabOptions, SIGNAL(currentChanged(int)), this, SLOT(preUpdateModel()));
+	connect(win->optionsUI->tabOptions, SIGNAL(currentChanged(int)), this, SLOT(chainLoadOptions()));
 }
 
 QVariant OptionUtils::getField(const QModelIndex &index, int field)
@@ -1659,11 +1659,11 @@ void OptionUtils::loadDefault(QString text)
 
 	//init mameOpts with default values
 	foreach (QString key, iniSettings.keys())
-	{
-		MameOption *pMameOpt = new MameOption(0);	//fixme parent
+		{
+				MameOption *pMameOpt = new MameOption(0);	//fixme parent
 		pMameOpt->defvalue = iniSettings.value(key);
 		mameOpts[key] = pMameOpt;
-	}
+					}
 
 	//take the first value of mame's default inipath csv as our inipath
 	QStringList inipaths = mameOpts["inipath"]->defvalue.split(";");
@@ -1678,7 +1678,7 @@ void OptionUtils::loadDefault(QString text)
 	if (iniFile.size() == 0)
 		iniFile.remove();
 
-	//mkdir for individual game settings
+	// mkdir for individual game settings
 	QDir().mkpath(mameIniPath + "ini/source");
 
 	iniFile.setFileName(mameIniPath + "ini/puckman" INI_EXT);
@@ -1714,7 +1714,7 @@ void OptionUtils::loadDefault(QString text)
 
 	loadTemplate();
 
-	//append GUI MESS extra software paths to mameOpts
+	// append GUI MESS extra software paths to mameOpts
 	foreach (QString gameName, pMameDat->games.keys())
 	{
 		GameInfo *gameInfo = pMameDat->games[gameName];
@@ -1727,6 +1727,7 @@ void OptionUtils::loadDefault(QString text)
 			hasDevices = true;
 		}
 	}
+
 	//append GUI and MESS settings to optCatMap
 	QStringList optNames = mameOpts.keys();
 	qSort(optNames);
@@ -1745,8 +1746,8 @@ void OptionUtils::loadDefault(QString text)
 		}
 		else
 			optCatMap["01_GUI Paths_00_" + QString(QT_TR_NOOP("GUI paths"))] << optName;
-		}
 	}
+}
 
 // assign option type, defvalue, min, max, etc. from template
 void OptionUtils::loadTemplate()
@@ -1854,7 +1855,7 @@ void OptionUtils::loadIni(int optLevel, const QString &iniFileName)
 				break;
 
 			case OPTLEVEL_CLONEOF:
-				pMameOpt->currvalue = pMameOpt->cloneofvalue = pMameOpt->biosvalue;
+				pMameOpt->currvalue = pMameOpt->cloneofvalue= pMameOpt->biosvalue;
 				break;
 
 			case OPTLEVEL_CURR:
@@ -2114,7 +2115,7 @@ QHash<QString, QString> OptionUtils::parseIni(QTextStream &in, bool isInitOptCat
 		{
 			//line contains only # is ignored
 			if (line.size() > 2 && isInitOptCatMap)
-			{
+		{
 				QString category(line);
 				category.remove("#");
 				category.remove("OPTIONS");
@@ -2132,7 +2133,7 @@ QHash<QString, QString> OptionUtils::parseIni(QTextStream &in, bool isInitOptCat
 		}
 		// ignore <UNADORNED>
 		else if (!line.startsWith("<") && line.size() > 0)
-		{
+			{
 			QStringList strs = utils->split2Str(line, " ");
 
 			//add option name to optCatMap
@@ -2141,37 +2142,34 @@ QHash<QString, QString> OptionUtils::parseIni(QTextStream &in, bool isInitOptCat
 
 			//option has a value from ini
 			if (strs.size() > 1)
-			{
+				{
 				key = strs.first();
 				value = strs.last();
 
-				//remove quoted value if needed
-				if (value.startsWith('"') && value.endsWith('"'))
+					//remove quoted value if needed
+					if (value.startsWith('"') && value.endsWith('"'))
+					{
+						value.remove(0, 1);
+						value.chop(1);
+					}
+				}
+			//option has empty value
+				else
 				{
-					value.remove(0, 1);
-					value.chop(1);
+				key = strs.first();
+					value = "";
 				}
 			}
-			//option has empty value
-			else
-			{
-				key = strs.first();
-				value = "";
-			}
-		}
 
 		if (!key.isEmpty())
 			settings[key] = value;
-	}
+		}
 
 	return settings;
 }
 
-//core:update mameOpts by loading .ini files of different levels
-//gui:determine what to update in the option dialog
-//method 0 updates model, method 1 loads ini only
 //fixme: use signal to separate GUI and core logic
-void OptionUtils::preUpdateModel(QListWidgetItem *currrentOptCatItem, int optLevel, QString gameName, int method)
+void OptionUtils::chainLoadOptions(QListWidgetItem *currrentOptCatItem, int optLevel, QString gameName, int method)
 {
 	if (gameName.isEmpty())
 		gameName = currentGame;
@@ -2196,9 +2194,6 @@ void OptionUtils::preUpdateModel(QListWidgetItem *currrentOptCatItem, int optLev
 		if (currrentOptCatItem != NULL)
 			optSubCat = currrentOptCatItem->text();
 	}
-
-	if (optSubCat.isEmpty())
-		return;
 
 	/* update mameOpts by loading .ini files of different levels */
 	GameInfo *gameInfo = pMameDat->games[gameName];
@@ -2551,4 +2546,3 @@ void OptionUtils::updateHeaderSize(int logicalIndex, int oldSize, int newSize)
 
 //	win->log(QString("header%3: %1 to %2").arg(optInfos[optLevel]->optView->header()->sectionSize(0)).arg(newSize).arg(logicalIndex));
 }
-

mameopt.h
@@ -181,7 +181,10 @@ public:
 	void saveIniFile(int , const QString &);
 
 public slots:
-	void preUpdateModel(QListWidgetItem *currItem = 0, int optLevel = -1, QString gameName = "", int method = 0);
+	//core:update mameOpts by loading .ini files of different levels
+	//gui:determine what to update in the option dialog
+	//method 0 updates model, method 1 loads ini only
+	void chainLoadOptions(QListWidgetItem *currItem = 0, int optLevel = -1, QString gameName = "", int method = 0);
 	void updateHeaderSize(int, int, int);
 
 private:

mamepgui.pro
@@ -67,7 +67,10 @@ TRANSLATIONS = \
 
 RESOURCES = mamepgui_main.qrc
 
-win32: RC_FILE = mamepgui.rc
+win32 {
+	RC_FILE = mamepgui.rc
+	QMAKE_LFLAGS_RELEASE += -static-libgcc
+}
 
 macx {
 	ICON = mamepgui.icns